8871a893bdd364b2ea415fc26b3968b1b5f3ba3e
adeline-honore <honore_adeline@yahoo.fr>
2021-11-23 14:09:44 +0100

    creation des 3 pages et gestion pour la page conversion

---
+++ 2021-11-baluchon/Controller/ChangeRateView.swift
@@ -0,0 +1,20 @@
+//
+//  ChangeRate.swift
+//  2021-11-baluchon
+//
+//  Created by HONORE Adeline on 23/11/2021.
+//
+
+import UIKit
+
+class ChangeRateView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ 2021-11-baluchon/Network/Network.swift
@@ -0,0 +1,8 @@
+//
+//  Network.swift
+//  2021-11-baluchon
+//
+//  Created by HONORE Adeline on 23/11/2021.
+//
+
+import Foundation
